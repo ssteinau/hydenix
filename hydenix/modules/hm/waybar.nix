76bf0c5dcb6151ff5750b4b6d510ea337dd3751f
Sebastian Steinau <sebastian.steinau+github@steinautech.com>
2026-01-06 23:09:10 +0100

    fix(waybar): correct python package reference for pyamdgpuinfo

--- hydenix/modules/hm/waybar.nix
+++ hydenix/modules/hm/waybar.nix
@@ -45,7 +45,7 @@ in
         playerctl # media player cli
         gobject-introspection # for python packages
         (python3.withPackages (ps: with ps; [ pygobject3 ])) # python with pygobject3
-        python-pyamdgpuinfo # AMD GPU information library
+        python3Packages.pyamdgpuinfo # AMD GPU information library
         lm_sensors # sensors information library
         power-profiles-daemon # power profiles daemon
       ];
