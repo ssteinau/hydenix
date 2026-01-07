042a1530601e16e380ed27d9e610d888c39f0944
Sebastian Steinau <sebastian.steinau+github@steinautech.com>
2026-01-07 12:00:36 +0100

    fix(waybar): uncomment includes.json and expand env vars for stable waybar

--- hydenix/sources/hyde.nix
+++ hydenix/sources/hyde.nix
@@ -62,8 +62,10 @@ pkgs.stdenv.mkDerivation {
     # Fix lines 20,23,27-38: all remaining float rules need windowrulev2
     sed -i '20s/^windowrule = /windowrulev2 = /' Configs/.local/share/hypr/windowrules.conf
     sed -i '23s/^windowrule = /windowrulev2 = /' Configs/.local/share/hypr/windowrules.conf
-    # Fix waybar config includes - replace env vars with actual paths
-    find Configs/.local/share/waybar/layouts -name "*.jsonc" -type f -print0 | xargs -0 sed -i 's|$XDG_CONFIG_HOME|~/.config|g; s|$XDG_DATA_HOME|~/.local/share|g'
+    # Fix waybar config includes - uncomment includes.json and fix JSON syntax
+    find Configs/.local/share/waybar/layouts -name "*.jsonc" -type f -exec sed -i 's|$XDG_CONFIG_HOME/waybar/modules/*json*"|$XDG_CONFIG_HOME/waybar/modules/*json*",|g; s|$XDG_DATA_HOME/waybar/modules/*json*"|$XDG_DATA_HOME/waybar/modules/*json*",|g; s|// "$XDG_CONFIG_HOME/waybar/includes/includes.json"|"$XDG_CONFIG_HOME/waybar/includes/includes.json"|g' {} ;
+    # Expand environment variables to absolute paths in waybar layouts
+    find Configs/.local/share/waybar/layouts -name "*.jsonc" -type f -exec sed -i 's|$XDG_CONFIG_HOME|$HOME/.config|g; s|$XDG_DATA_HOME|$HOME/.local/share|g' {} ;
     # Fix lines 66-77: all layerrule need layerrulev2
     sed -i '66,77s/^layerrule = /layerrulev2 = /' Configs/.local/share/hypr/windowrules.conf
     sed -i '27,38s/^windowrule = /windowrulev2 = /' Configs/.local/share/hypr/windowrules.conf
